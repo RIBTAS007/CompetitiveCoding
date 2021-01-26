6d50e1f62cc5441a30d406ccf97dd985d02cb157
ribtas007 <ribtas007@outlook.com>
2021-01-26 13:56:04 +0530

    print-z-official updated

---
+++ Getting Started/01-print-z-official.cpp
@@ -0,0 +1,24 @@
+/* You are required to print a 'z' of size 5 using '*'. */
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **agrc)
+{
+    //write your code here
+    cout << "*****" << endl;
+    cout << "   * " << endl;
+    cout << "  *  " << endl;
+    cout << " *   " << endl;
+    cout << "*****" << endl;
+
+    return 0;
+}
+
+/*
+
+Notes :
+         
+cout is used to print statements in C++
+endl is used to move to next line
+
+*/
